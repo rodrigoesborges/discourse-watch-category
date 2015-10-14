5e23bb6f68ba141d2d4e04c0b2fb84b4f652c130
Tobias Eigen <tobiaseigen@gmail.com>
2015-10-14 09:17:25 -0700

    added indication for start/end for each category

--- plugin.rb
+++ plugin.rb
@@ -4,10 +4,19 @@
 # authors: Jared Needell
 
 module ::WatchCategory
+<<<<<<< HEAD
   def self.watch_by_group(category_slug, group_name)
     category = Category.find_by(slug: category_slug)
     group = Group.find_by_name(group_name)
     return if category.nil? || group.nil?
+=======
+  def self.watch_category!
+
+# *start* ANNOUNCEMENTS watched by NAMATI_STAFF
+    announcements_category = Category.find_by_slug("announcements")
+    namati_staff_group = Group.find_by_name("namati_staff")
+    return if announcements_category.nil? || namati_staff_group.nil?
+>>>>>>> 2b6ceca (added indication for start/end for each category)
 
     group.users.each do |user|
       watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
@@ -33,7 +42,9 @@ module ::WatchCategory
       watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
       CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], announcements_category.id) unless watched_categories.include?(announcements_category.id)
     end
+# *end* ANNOUNCEMENTS
 
+# *start* LEADERSHIP watched by NAMATI_LEADERSHIP
     leadership_category = Category.find_by_slug("leadership")
     namati_leadership_group = Group.find_by_name("namati_leadership")
     return if leadership_category.nil? || namati_leadership_group.nil?
@@ -42,7 +53,9 @@ module ::WatchCategory
       watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
       CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], leadership_category.id) unless watched_categories.include?(leadership_category.id)
     end
+# *end* LEADERSHIP
 
+# *start* NGC watched by NETWORK_LEADERSHIP 
     ngc_category = Category.find_by_slug("ngc")
     network_ngc_group = Group.find_by_name("network_ngc")
     return if ngc_category.nil? || ngc_group.nil?
@@ -51,6 +64,8 @@ module ::WatchCategory
       watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
       CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], ngc_category.id) unless watched_categories.include?(ngc_category.id)
     end
+# *end* NGC
+
   end
 end
 
