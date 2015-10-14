6359faa58ff31e7fb36d2a0ce5a293856689e214
Tobias Eigen <tobiaseigen@gmail.com>
2015-10-13 17:12:40 -0700

    customized to namati needs
    namati_staff will now follow announcements
    namati_leadership will now follow leadership

--- plugin.rb
+++ plugin.rb
@@ -7,37 +7,26 @@
 
 module ::WatchCategory
   def self.watch_category!
-    mcneel_private_category = Category.find_by_slug("mcneel-private")
-    mcneel_group = Group.find_by_name("mcneel")
+    announcements_category = Category.find_by_slug("announcements")
+    namati_staff_group = Group.find_by_name("namati_staff")
+    return if announcements_category.nil? || namati_staff_group.nil?
 
-    unless mcneel_private_category.nil? || mcneel_group.nil?
-      mcneel_group.users.each do |user|
-        watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
-        if watched_categories.exclude?(mcneel_private_category.id)
-          CategoryUser.set_notification_level_for_category(
-            user,
-            CategoryUser.notification_levels[:watching],
-            mcneel_private_category.id,
-          )
-        end
-      end
-    end
-
-    reseller_category = Category.find_by_slug("resellers")
-    reseller_group = Group.find_by_name("resellers")
-    return if reseller_category.nil? || reseller_group.nil?
-
-    reseller_group.users.each do |user|
+    namati_staff_group.users.each do |user|
       watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
-      if watched_categories.exclude?(reseller_category.id)
-        CategoryUser.set_notification_level_for_category(
-          user,
-          CategoryUser.notification_levels[:watching],
-          reseller_category.id,
-        )
-      end
+      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], announcements_category.id) unless watched_categories.include?(announcements_category.id)
     end
   end
+
+    leadership_category = Category.find_by_slug("leadership")
+    namati_leadership_group = Group.find_by_name("namati_leadership")
+    return if leadership_category.nil? || namati_leadership_group.nil?
+
+    namati_leadership_group.users.each do |user|
+      watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], leadership_category.id) unless watched_categories.include?(leadership_category.id)
+    end
+  end
+
 end
 
 after_initialize do
