a28a9ba8da7b33aca33bef2d869876f4ebb8000a
Tobias Eigen <tobiaseigen@gmail.com>
2015-10-28 06:07:15 -0700

    added working groups

--- plugin.rb
+++ plugin.rb
@@ -33,6 +33,42 @@ module ::WatchCategory
       watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
       CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], ngc_category.id) unless watched_categories.include?(ngc_category.id)
     end
+
+    culture-systems_category = Category.find_by_slug("culture-systems")
+    namati_culture_group = Group.find_by_name("namati_culture")
+    return if culture-systems_category.nil? || namati_culture_group.nil?
+
+    namati_culture_group.users.each do |user|
+      watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], culture-systems_category.id) unless watched_categories.include?(culture-systems_category.id)
+    end
+
+    vision-outcome_category = Category.find_by_slug("vision-outcome")
+    namati_visionoutcome_group = Group.find_by_name("namati_visionoutcome")
+    return if vision-outcome_category.nil? || namati_visionoutcome_group.nil?
+
+    namati_visionoutcome_group.users.each do |user|
+      watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], vision-outcome_category.id) unless watched_categories.include?(vision-outcome_category.id)
+    end  
+
+    assessingopp_category = Category.find_by_slug("assessingopp")
+    namati_assessingopp_group = Group.find_by_name("namati_assessingopp")
+    return if assessingopp_category.nil? || namati_assessingopp_group.nil?
+
+    namati_assessingopp_group.users.each do |user|
+      watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], assessingopp_category.id) unless watched_categories.include?(assessingopp_category.id)
+    end
+
+    scale_category = Category.find_by_slug("scale")
+    namati_scale_group = Group.find_by_name("namati_scale")
+    return if scale_category.nil? || namati_scale_group.nil?
+
+    namati_scale_group.users.each do |user|
+      watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], scale_category.id) unless watched_categories.include?(scale_category.id)
+    end  
   end
 end
 
