73e0a873d3647688a184731067ff203d78628704
Jared Needell <jdn1582@rit.edu>
2016-03-23 21:12:58 -0400

    changed code for enforcing watch for all users

--- plugin.rb
+++ plugin.rb
@@ -6,23 +6,21 @@
 module ::WatchCategory
   def self.watch_category!
     announcements_category = Category.find_by(slug: "confidential-employees-only")
-    namati_staff_group = Group.find_by_name("EmployeesOnly")
-    return if announcements_category.nil? || namati_staff_group.nil?
+    employee_staff_group = Group.find_by_name("EmployeesOnly")
+    return if announcements_category.nil? || employee_staff_group.nil?
 
-    namati_staff_group.users.each do |user|
+    employee_staff_group.users.each do |user|
       watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
       CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], announcements_category.id) unless watched_categories.include?(announcements_category.id)
     end
 
     thepit_category = Category.find_by(slug: "the-pit")
-    everyone_group = Group.find_by_name("everyone")
-    return if thepit_category.nil? || everyone_group.nil?
 
-    everyone_group.users.each do |user|
+    User.all.each do |user|
       watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
       CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], thepit_category.id) unless watched_categories.include?(thepit_category.id)
-    end
-    
+    end  
+
   end
 end
 
