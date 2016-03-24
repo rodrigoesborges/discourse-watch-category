9426767d3a5f33223338403b80499b1c49924cec
Jared Needell <jdn1582@rit.edu>
2016-03-24 13:48:19 -0400

    Method not working

--- plugin.rb
+++ plugin.rb
@@ -5,27 +5,26 @@
 # authors: Jared Needell
 
 module ::WatchCategory
-  def watch_by_group(category_slug, group_name)
-    category = Category.find_by(slug: category_slug)
-    group = Group.find_by_name(group_name)
-    return if category.nil? || group.nil?
-
-    group.users.each do |user|
-      watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
-      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], category.id) unless watched_categories.include?(category.id)
-    end
-  end
-
-  def watch_all(category_slug)
-    category = Category.find_by(slug: category_slug)
-
-    User.all.each do |user|
-      watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
-      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], category.id) unless watched_categories.include?(category.id)
-    end 
-  end
-
   def self.watch_category!
+
+    def watch_by_group(category_slug, group_name)
+      category = Category.find_by(slug: category_slug)
+      group = Group.find_by_name(group_name)
+      return if category.nil? || group.nil?
+
+      group.users.each do |user|
+        watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+        CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], category.id) unless watched_categories.include?(category.id)
+      end
+    end
+
+    def watch_all(category_slug)
+      category = Category.find_by(slug: category_slug)
+      User.all.each do |user|
+        watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+        CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], category.id) unless watched_categories.include?(category.id)
+      end 
+    end
 
     watch_by_group("confidential-employees-only", "EmployeesOnly")
     watch_by_group("facilities-us","Morrisville")
