27e10cc48ce8ac31d95f67f60aba6098b156e139
Jared Needell <jdn1582@rit.edu>
2016-03-24 14:07:18 -0400

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -5,42 +5,35 @@
 # authors: Jared Needell
 
 module ::WatchCategory
+  def watch_by_group(category_slug, group_name)
+    category = Category.find_by(slug: category_slug)
+    group = Group.find_by_name(group_name)
+    return if category.nil? || group.nil?
+
+    group.users.each do |user|
+      watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], category.id) unless watched_categories.include?(category.id)
+    end
+  end
+
+  def watch_all(category_slug)
+    category = Category.find_by(slug: category_slug)
+    User.all.each do |user|
+      watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], category.id) unless watched_categories.include?(category.id)
+    end 
+  end
 
   def self.watch_category!
+    WatchCategory.watch_by_group("confidential-employees-only", "EmployeesOnly")
+    WatchCategory.watch_by_group("facilities-us","Morrisville")
+    WatchCategory.watch_by_group("facilities-us","Seattle")
+    WatchCategory.watch_by_group("facilities-happenings","RemoteUS")
+    WatchCategory.watch_by_group("facilities-uk","London")
 
-    def watch_by_group(category_slug, group_name)
-      category = Category.find_by(slug: category_slug)
-      group = Group.find_by_name(group_name)
-      return if category.nil? || group.nil?
-
-      group.users.each do |user|
-        watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
-        CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], category.id) unless watched_categories.include?(category.id)
-      end
-    end
-
-    def watch_all(category_slug)
-      category = Category.find_by(slug: category_slug)
-      User.all.each do |user|
-        watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
-        CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], category.id) unless watched_categories.include?(category.id)
-      end 
-    end
-
-    WatchCategory.watch_by_group("capitulo-brasil","capitulo_brasil")
-
-    watch_by_group("confidential-employees-only", "EmployeesOnly")
-    watch_by_group("facilities-us","Morrisville")
-    watch_by_group("facilities-us","Seattle")
-    watch_by_group("facilities-happenings","RemoteUS")
-    watch_by_group("facilities-uk","London")
-
-    watch_all("company-announcements")
-    watch_all("Corporate-Scheduled-Maintenance")
-    watch_all("Corporate-System-Status")
-
- 
-
+    WatchCategory.watch_all("company-announcements")
+    WatchCategory.watch_all("Corporate-Scheduled-Maintenance")
+    WatchCategory.watch_all("Corporate-System-Status")
   end
 end
 
