a030035c3957973c2b4a6cb40b82f1cb508c5810
Jared Needell <jdn1582@rit.edu>
2016-04-29 08:44:22 -0400

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -12,7 +12,7 @@ module ::WatchCategory
 
     group.users.each do |user|
       watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
-      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], category.id) unless watched_categories.include?(category.id) || !user.staged
+      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], category.id) unless watched_categories.include?(category.id) || user.staged
     end
   end
 
@@ -20,7 +20,7 @@ module ::WatchCategory
     category = Category.find_by(slug: category_slug)
     User.all.each do |user|
       watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
-      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], category.id) unless watched_categories.include?(category.id)  || !user.staged
+      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], category.id) unless watched_categories.include?(category.id)  || user.staged
     end 
   end
 
